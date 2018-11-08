b984cf78bc1de342d3999cda45fd2dc3d9021b4b
kubzdel <kubzdel@gmail.com>
2018-11-08 11:26:17 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,151 @@
+from __future__ import division, print_function, absolute_import
+
+import datetime
+
+import tensorflow as tf
+import tflearn
+from scipy._lib.six import xrange
+from tflearn.data_utils import shuffle
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+import random as rand
+from skimage import data as img
+from PIL import Image
+from numpy import array
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+SAMPLE_EXTENSION_LENGHT = 4
+TEST_COLUMN_SIZE = 6
+SINGLE_ANS = 2
+def chunkify(lst, n):
+    return [lst[i::n] for i in xrange(n)]
+
+def prepareY(answers):
+    Y = []
+    for ans in answers:
+        vector = []
+        if ans == 1:
+            vector = [1,0,0,0]
+        if ans == 2:
+            vector = [0,1,0,0]
+        if ans == 3:
+            vector = [0,0,1,0]
+        if ans == 4:
+            vector = [0,0,0,1]
+        Y.append(vector)
+
+    return Y
+
+
+
+def addAnswers(Y):
+    for y in range(0,SINGLE_ANS):
+        for i in range(0,TEST_COLUMN_SIZE):
+            Y.append([1,0,0,0])
+        for i in range(0, TEST_COLUMN_SIZE):
+            Y.append([0, 1, 0, 0])
+        for i in range(0, TEST_COLUMN_SIZE):
+            Y.append([0, 0, 1, 0])
+        for i in range(0, TEST_COLUMN_SIZE):
+            Y.append([0, 0, 0, 1])
+    return Y
+
+images = []
+masks = []
+k = 10
+X = []
+Y = []
+Xtest = []
+Ytest = []
+MASK_SIZE = 25  # musi być nieparzysta
+PHOTO_SAMPLES = 1000
+# ładuj zdjęcia
+directory = os.getcwd() + "\samples" + '\\'
+for folder in os.listdir("samples"):
+    folderConent = os.listdir("samples\\"+folder)
+    folderConent = sorted(folderConent)
+    for file in folderConent:
+        pngfile = img.load(directory + folder+"\\"+file, True)
+        pngfile = pngfile/255
+        X.append(array(pngfile))
+    Y = addAnswers(Y)
+plt.imshow(array(X[1]))
+plt.show()
+x_size,y_size = X[0].shape
+
+answers = [1,1,2,2,3,2,4,4,1,1,4,2,3,3,3,4,1,2,1,1,4,2,3,2]
+X, Y = shuffle(X, Y)
+
+averageAcc = 0
+
+trainSamples = []
+testSamples = []
+
+trainMasks = []
+testMasks = []
+
+Xtrain = X[:(int(len(X)/5)+1) * 4]
+#Xtrain = Xtrain.reshape(Xtrain.shape[0],x_size,y_size,1)
+Ytrain = Y[:(int(len(X)/5)+1) * 4]
+Xtest = X[(int(len(X)/5)+1) * 4:]
+#Xtest =  Xtest.reshape(Xtest.shape[0],x_size,y_size,1)
+Ytest = Y[(int(len(X)/5)+1) * 4:]
+
+
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+network = input_data(shape=[x_size, y_size],
+                     name='InputData')
+print(network)
+# network = conv_2d(network, 8, 16, activation='relu')
+# #
+# network = max_pool_2d(network, 2)
+#
+# network = conv_2d(network, 8, 3, activation='relu')
+#
+#network = conv_2d(network, 64, 3, activation='relu')
+#
+# network = max_pool_2d(network, 2)
+
+network = fully_connected(network, 64, activation='relu')
+
+network = dropout(network, 0.01)
+
+network = fully_connected(network, 4, activation='softmax')
+
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.01)
+
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(Xtrain, Ytrain, n_epoch=500, shuffle=True, validation_set=(Xtest, Ytest),
+          show_metric=True, batch_size=16, snapshot_epoch=False)
+
+accuracy = 0
+
+for toPredict, actual in zip(Xtest, Ytest):
+    prediction = model.predict([toPredict])
+    predicted_class = np.argmax(prediction)
+    actual_class = np.argmax(actual)
+    if (predicted_class == actual_class):
+        accuracy += 1
+accuracy = accuracy / len(Ytest)
+averageAcc += accuracy
+print("Acucracy: " + str(accuracy))
+
+model.save("docelowy4/" + str(accuracy) + " eye-veins-classifier.tfl")
+
+print("Network trained and saved as eye-veinsclassifier.tfl!")
+
+
+averageAcc = averageAcc / k
+print(averageAcc)
+text_file = open("docelowy4/averageACC.txt", "w")
+text_file.write("Średnia dokładność: " + str(averageAcc))
+text_file.close()
---
+++ getBoxes.py
@@ -0,0 +1,72 @@
+import os
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def standarizeName(x,y,dim):
+    xText = str(x)
+    yText = str(y)
+    maks = len(str(dim))
+    while(x%40!=0):
+        x+=1
+    while (y % 40 != 0):
+        y+=1
+    rightPad = maks - len(xText)
+    padding1 = ""
+    for i in range(0, rightPad):
+        padding1 += '0'
+    rightPad2 = maks - len(yText)
+    padding2 = ""
+    for i in range(0, rightPad2):
+        padding2 += '0'
+
+    return padding1+str(x)+"_"+padding2+str(y)
+
+FONT_SIZE = 12
+BORDER_SIZE = 300
+for file in os.listdir("scans"):
+    img_rgb = cv2.imread("scans/"+file)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    x_dim, y_dim = img_gray.shape
+    template = cv2.imread('indicator.png',0)
+    template =  cv2.resize(template, (58, 43))
+
+    w, h = template.shape[:2]
+    w = 130
+    h = 350
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.7
+    loc = np.where( res >= threshold)
+    outputImage = cv2.copyMakeBorder(
+        img_gray,
+        BORDER_SIZE,
+        BORDER_SIZE,
+        BORDER_SIZE,
+        BORDER_SIZE,
+                     cv2.BORDER_CONSTANT,
+                     value=(255,255,255)
+                  )
+
+    mask = np.zeros(outputImage.shape[:2], np.uint8)
+
+    newDir = os.getcwd() + "/samples/" + file.split('.')[0]
+    if not os.path.exists(newDir):
+        os.makedirs(newDir)
+
+    for pt in zip(*loc[::-1]):
+        if mask[pt[1] + int(h / 2), pt[0] + int(w / 2)] != 255:
+            mask[pt[1]:pt[1] + h, pt[0]:pt[0] + w] = 255
+            lst = list(pt)
+            lst[0] =lst[0]-50+BORDER_SIZE
+            lst[1] +=(55+BORDER_SIZE)
+            pt = tuple(lst)
+
+            cv2.rectangle(outputImage, pt, (pt[0] + 130, pt[1] + 350), (0,0,255), 2)
+            crop_img = outputImage[pt[1]:pt[1]+350,pt[0]:pt[0]+130]
+           # cv2.imshow("cropped", crop_img)
+            name = standarizeName(pt[0],pt[1],max(x_dim,y_dim))
+            crop_img = cv2.resize(crop_img,None,fx=0.2,fy=0.2)
+            cv2.imwrite(newDir+"/sample_"+name+".png",crop_img)
+
+cv2.imwrite('res.png',outputImage)
